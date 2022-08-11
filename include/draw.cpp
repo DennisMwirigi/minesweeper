5b30a707dab1a091e0c03effc458263aa5a741cd
Dennis Arimi <dennisarimi@uvic.ca>
2022-08-11 15:06:42 -0700

    Drawing 9x16 grid

--- include/draw.cpp
+++ include/draw.cpp
@@ -4,22 +4,22 @@
 #include <iostream>
 #include <vector>
 
-int rows, columns = 9;
-int cell_height, cell_width = 16;
+int rows = 9, columns = 9;
+int cell_height = 16, cell_width = 16;
 
 void drawGrid(int x, int y)
 {
-    glColor3f(0.8f, 0.8f, 0.8f);
+    glColor3f(0.0f, 0.0f, 0.8f);
     glBegin(GL_QUADS);
-    glVertex2f(x * rows, y * columns);
-    glVertex2f((x + 1) * rows, y * columns);
-    glVertex2f((x + 1) * rows, (y + 1) * columns);
-    glVertex2f(x * rows, (y + 1) * columns);
+    glVertex2f(x * cell_width, y * cell_height);
+    glVertex2f((x + 1) * cell_width, y * cell_height);
+    glVertex2f((x + 1) * cell_width, (y + 1) * cell_height);
+    glVertex2f(x * cell_width, (y + 1) * cell_height);
     glEnd();
 
     glColor3f(1.0f, 1.0f, 1.0f);
     glBegin(GL_LINES);
-    glVertex2f(x * cell_width, y * cell_height);
+    glVertex2f((x * cell_width), (y * cell_height));
     glVertex2f((x + 1) * cell_width - 1, y * cell_height);
     glVertex2f(x * cell_width, y * cell_height);
     glVertex2f(x * cell_width, (y + 1) * cell_height - 1);
@@ -32,16 +32,6 @@ void drawGrid(int x, int y)
     glVertex2f(x * cell_width, (y + 1) * cell_height - 1);
     glVertex2f((x + 1) * cell_width - 1, (y + 1) * cell_height - 1);
     glEnd();
-
-    // std::vector<std::vector<int>> board(rows, std::vector<int>(columns, 0));
-    // for (int i = 0; i < rows; i++)
-    // {
-    //     for (int j = 0; j < columns; j++)
-    //     {
-    //         std::cout << board[i][j];
-    //     }
-    //     std::cout << std::endl;
-    // }
 }
 
 void draw()
@@ -55,50 +45,44 @@ void draw()
     }
 }
 
+void reshape(int w, int h)
+{
+    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, rows * cell_height, columns * cell_width, 0, -1.0, 1.0);
+    glMatrixMode(GL_MODELVIEW);
+}
+
 int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 
-    int width = 640;
-    int height = 480;
-    glutInitWindowSize(width, height);
-    // glutInitWindowSize(columns * cell_width, rows * cell_height);
-
-    // int x = 200;
-    // int y = 100;
-    glutInitWindowPosition(200, 100);
+    // glutInitWindowSize(640, 480);
+    glutInitWindowSize(columns * cell_width, rows * cell_height);
+    glutInitWindowPosition(100, 120);
 
     glutCreateWindow("Minesweeper");
-    // drawGrid(5, 8);
-
-    // GLclampf Red = 0.0f, Green = 0.0f, Blue = 0.0f, Alpha = 1.0f;
-    // glClearColor(Red, Green, Blue, Alpha);
-    // glClearColor(0, 0, 0, 1);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(0, rows * cell_height, columns * cell_width, 0, -1.0, 1.0);
 
     glutDisplayFunc(
         []()
         {
-            glClear(GL_COLOR_BUFFER_BIT);
             std::cout << "drawing grid...\n";
 
-            glColor3f(0.8f, 0.8f, 0.8f);
-            glBegin(GL_QUADS);
-            glVertex2f(-0.5, 0.5);
-            glVertex2f(0.5, 0.5);
-            glVertex2f(0.5, -0.5);
-            glVertex2f(-0.5, -0.5);
-            glEnd();
+            glClear(GL_COLOR_BUFFER_BIT);
+            glLoadIdentity();
+
+            draw();
 
             glutSwapBuffers();
+            glFlush();
         });
-    glClearColor(0, 0, 0, 1);
-    glutMainLoop();
+    glutReshapeFunc(reshape);
 
-    // drawGrid(5, 8);
+    glClearColor(0, 0, 0, 1);
+
+    glutMainLoop();
 
     return 0;
 }
